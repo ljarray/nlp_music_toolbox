42a29fcb7da850340992f3b004edffbb641eddbf
ljarray <laura-jane@Laura-Janes-MacBook-Pro.local>
2016-07-17 13:36:28 -0500

    adds csv reader to CsvRdfConverter.java
    - adds documentation to getNote method
    - fixes logical error in not a note value check
    - adds csv reader, directory traversal
    - adds functionality to pull notes from csv files
    - moves data directory out of code packages

--- src/nlp/music/Main.java
+++ src/nlp/music/Main.java
@@ -1,9 +1,18 @@
 package nlp.music;
 
+import nlp.music.tools.CsvRdfConverter;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // write your code here
 
+        CsvRdfConverter classics = new CsvRdfConverter();
+        classics.convertSongs(new File("data/csv/bach"));
     }
 }
+
+
--- src/nlp/music/tools/CsvRdfConverter.java
+++ src/nlp/music/tools/CsvRdfConverter.java
@@ -1,6 +1,12 @@
 package nlp.music.tools;
 
+import java.io.File;
 import java.lang.Math;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CsvRdfConverter {
 
@@ -16,16 +22,45 @@ public class CsvRdfConverter {
      *
      */
 
+
     public static String getNote(int num){
-
-    String note = "";
-
+        // Given a number 0 - 127 read from a csv file in the midi specification, returns the unique note and the octave
         String[] notesInAnOctave = {"C", "C#", "D", "D#", "E","F", "F#","G", "G#", "A", "A#", "B"};
-        // Find the note based on number (they are mapped from C - B, and cycle every 12 numbers)
-        if ( 0 >= num && num < 128) {
-            note = notesInAnOctave[num % 12] + ((int) Math.floor(num / 12));
-        } else {/*Throw an error, message "Note number from midi file is out of bounds. Must be between 0 - 127.*/}
 
-        return note;
+        // Find the note based on number (they are mapped from C - B, and cycle every 12 numbers)
+        if ( 0 <= num && num < 128) {
+            return notesInAnOctave[num % 12] + ((int) Math.floor(num / 12));
+        } else {
+            /*Throw an error, message "Note number from midi file is out of bounds. Must be between 0 - 127.*/
+            return "Error: not a note";
+        }
+    }
+
+    public void convertSongs(File f) throws IOException, NullPointerException {
+
+        Pattern notePattern = Pattern.compile("note_on_c", Pattern.CASE_INSENSITIVE);
+
+        if (f.isDirectory()) {
+            for (File ff : f.listFiles()) {
+                convertSongs(ff);
+            }
+        }
+        else {
+            // Reads input from a csv song file, and converts the song into rdf objects
+            String thisLine;
+            try {
+                BufferedReader csvReader = new BufferedReader(new FileReader(f.getAbsolutePath()));
+                while ((thisLine = csvReader.readLine()) != null) {
+                    Matcher m = notePattern.matcher(thisLine);
+                    if (m.find()){
+                        System.out.print(thisLine.split("\\s*,\\s*")[4] + ", ");
+                        System.out.println(getNote(Integer.valueOf(thisLine.split("\\s*,\\s*")[4])));
+                        //System.out.print(thisLine + ", ");
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
