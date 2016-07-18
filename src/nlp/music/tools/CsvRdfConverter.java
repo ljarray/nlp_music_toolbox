7019d38e64faa9499760da472f777b5d3ec74752
ljarray <laura-jane@Laura-Janes-MacBook-Pro.local>
2016-07-17 19:30:17 -0500

    adds Song and Note classes
    - adds Song class object
    - adds Note class object
    - provides custom objects for the CsvRdfConverter class to reference

--- src/nlp/music/tools/CsvRdfConverter.java
+++ src/nlp/music/tools/CsvRdfConverter.java
@@ -5,25 +5,25 @@ import java.lang.Math;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CsvRdfConverter {
+class Note {
+    String note;
+    Integer time;
+    Integer channel;
 
-    /**
-     *  HIGH Priority
-     *
-     *  todo: Read csv files
-     *  todo: Read note values from lines with the notes_on_c value (3rd column)
-     *  todo: Create rdf triple with {note 1} {inChordWith} {note 2} schema for notes that appear together
-     *  todo: Create rdf triple with {note 1} {followedBy} {note 2} schema for a sequence of two notes
-     *  todo: Create rdf triple with {note 2} {precededBy} {note 1} schema for a sequence of two notes
-     *  done: Convert note numbers into alphanumeric notation
-     *
-     */
+    public Note (String line){
+        String[] cols = line.split("\\s*,\\s*");
 
+        channel = Integer.valueOf(cols[0]);
+        time = Integer.valueOf(cols[1]);
+        note = setNote(Integer.valueOf(cols[4]));
+    }
 
-    public static String getNote(int num){
+    public static String setNote(int num){
         // Given a number 0 - 127 read from a csv file in the midi specification, returns the unique note and the octave
         String[] notesInAnOctave = {"C", "C#", "D", "D#", "E","F", "F#","G", "G#", "A", "A#", "B"};
 
@@ -36,9 +36,40 @@ public class CsvRdfConverter {
         }
     }
 
-    public void convertSongs(File f) throws IOException, NullPointerException {
+    public Integer getChannel(){ return channel; }
 
-        Pattern notePattern = Pattern.compile("note_on_c", Pattern.CASE_INSENSITIVE);
+    public Integer getTime(){ return time; }
+
+    public String getNote(){ return note; }
+}
+
+class Song {
+
+    ArrayList<Integer> timeIndices = new ArrayList<>();
+    HashMap<Integer, ArrayList<Note>> notesByTime = new HashMap<>();
+
+    public Song (){}
+
+    public void addNote(String line){
+        Note n = new Note(line);
+        if ( !timeIndices.contains(n.getTime()) )
+           timeIndices.add(n.getTime());
+
+        if ( !notesByTime.containsKey(n.getTime()) )
+            notesByTime.put(n.getTime(), new ArrayList());
+
+        if ( !notesByTime.get(n.getTime()).contains(n))
+            notesByTime.get(n.getTime()).add(n);
+    }
+
+}
+
+public class CsvRdfConverter {
+
+    public static void convertSongs(File f) throws IOException, NullPointerException {
+
+        Pattern notePattern = Pattern.compile("note_on_c", Pattern.CASE_INSENSITIVE); // matches lines with Note_on_c
+        Song currentSong = new Song();
 
         if (f.isDirectory()) {
             for (File ff : f.listFiles()) {
@@ -48,19 +79,29 @@ public class CsvRdfConverter {
         else {
             // Reads input from a csv song file, and converts the song into rdf objects
             String thisLine;
+
             try {
                 BufferedReader csvReader = new BufferedReader(new FileReader(f.getAbsolutePath()));
                 while ((thisLine = csvReader.readLine()) != null) {
                     Matcher m = notePattern.matcher(thisLine);
-                    if (m.find()){
-                        System.out.print(thisLine.split("\\s*,\\s*")[4] + ", ");
-                        System.out.println(getNote(Integer.valueOf(thisLine.split("\\s*,\\s*")[4])));
-                        //System.out.print(thisLine + ", ");
+                    if (m.find() && !thisLine.split("\\s*,\\s*")[5].equals("0")){ // lines with the Note_on_c attribute && notes with nonzero volume
+                        currentSong.addNote(thisLine);
                     }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+            /**
+             *  High Priority
+             *
+             *  todo: take the notes in currentSong and add them into the RDF framework
+             *  todo: Create rdf triple with {note 1} {inChordWith} {note 2} schema for notes that appear together
+             *  todo: Create rdf triple with {note 1} {followedBy} {note 2} schema for a sequence of two notes
+             *  todo: Create rdf triple with {note 2} {precededBy} {note 1} schema for a sequence of two notes
+             *
+             */
+
         }
     }
 }
